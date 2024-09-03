dec2fa11f4e9cbfa1e1839306b191d2bacf84ec3
RalphOps <156088348+RalphOps@users.noreply.github.com>
2024-09-03 21:35:31 +0100

    Update swap.c
    03/09/2024 Assignment for checking size/length of an array

--- problem_sets/swap.c
+++ problem_sets/swap.c
@@ -5,18 +5,21 @@ int main (void)
 	int a[] = {2, 3, 10, 1, 9, 8};
 	int b[] = {3, 2, 10, 9, 8};
 	
+	//This line Gives the size/amount/lenght of the first array
 	int size = sizeof(a) / sizeof(a[0]);
 	
-	
+	//Same goes for the second array. To Get the size/lenght
 	int size2 = sizeof(b) / sizeof(b[0]);
 
  	printf("THE AMOUNT OF NUMBERS IN THE FIRST ARRAY : %i\n", size);
  	printf("THE AMOUNT OF NUMBERS IN THE SSCOND ARRAY : %i", size2);
    printf("\n"); 
-   
+
+	//This line loops through the arrays
    printf("THE COMMON ELEMNTS ARE : ");
   for (int i = 0; i <= size; i++) {
   for (int j = 0; j <= size2; j++) {
+	  //If statement to check which elements are simmilar
   if ( a[i] == b[j]) {
   printf("%d ", a[i]);
   
